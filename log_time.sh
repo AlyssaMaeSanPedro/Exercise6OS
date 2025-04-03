9cd73e0b208547ce7bf7ae35b6415b6bff55ebcd
AlyssaMaeSanPedro <163331805+AlyssaMaeSanPedro@users.noreply.github.com>
2025-04-03 14:37:14 +0000

    Act6

--- log_time.sh
+++ log_time.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "Current Date and Time: $(TZ=Asia/Manila date '+%a %b %d, %Y %H:%M:%S')" >> log.txt
+GITHUB_USER=$GITHUB_ACTOR
+echo "Current Date and Time: $(TZ=Asia/Manila date '+%a %b %d, %Y %H:%M:%S') - Logged by: $GITHUB_USER" >> log.txt
+
+COUNT=$(grep -c '^Current' log.txt)
+
+sed -i '$ d' log.txt 
+echo "Total logs recorded: $COUNT" >> log.txt
