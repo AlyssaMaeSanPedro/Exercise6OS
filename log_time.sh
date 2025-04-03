5b021f8e2529ca28e1353871bc34cadf9506e61c
AlyssaMaeSanPedro <163331805+AlyssaMaeSanPedro@users.noreply.github.com>
2025-04-03 15:19:45 +0000

    test6

--- log_time.sh
+++ log_time.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-GITHUB_USER=$GITHUB_ACTOR
+GITHUB_USER=$(echo $GITHUB_REPOSITORY | cut -d '/' -f1)
 
-echo "Current Date and Time: $(date '+%a %b %d, %Y %H:%M:%S') - Logged by: $GITHUB_USER" >> log.txt
+echo "Current Date and Time: $(TZ=Asia/Manila date '+%a %b %d, %Y %H:%M:%S') - Logged by: $GITHUB_USER" >> log.txt
+
 COUNT=$(grep -c '^Current' log.txt)
-# Be careful with sed -i as it can behave differently on different systems
-# Let's use a more reliable approach
+
 tail -n +1 log.txt | grep -v "Total logs recorded" > temp_log.txt
 echo "Total logs recorded: $COUNT" >> temp_log.txt
 mv temp_log.txt log.txt
